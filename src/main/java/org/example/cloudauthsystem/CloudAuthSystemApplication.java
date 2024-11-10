0529363c9de4af559242ac602b8e8f573baa66e8
tigerokuma <okanagantiger@gmail.com>
2024-11-09 16:18:50 -0800

    feat: Created a repo for cloud auth system

---
+++ src/main/java/org/example/cloudauthsystem/CloudAuthSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.cloudauthsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAuthSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudAuthSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cloudauthsystem/CloudAuthSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cloudauthsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudAuthSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
